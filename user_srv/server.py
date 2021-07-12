9794e3b5227133ae464cd39ae0a4dda2353cf9cb
xiangnan0718 <xiangnan0718@gmail.com>
2021-07-12 17:33:26 +0800

    服务优雅退出

--- user_srv/server.py
+++ user_srv/server.py
@@ -6,14 +6,25 @@
 @Version     : python 3.8.5
 @Description : 
 """
+import os
+import sys
+import signal
 import logging
 from concurrent import futures
 
 import grpc
 from loguru import logger
 
+BASE_DIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, BASE_DIR)
+
 from user_srv.proto import user_pb2, user_pb2_grpc
 from user_srv.handler.user import UserServicer
+
+
+def on_exit(signal, frame):
+    logger.error(f"进程中断！")
+    sys.exit(0)
 
 
 def serve():
@@ -21,6 +32,10 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     user_pb2_grpc.add_UserServicer_to_server(UserServicer(), server)
     server.add_insecure_port('[::]:8078')
+
+    # 主进程退出信号监听 并优雅退出
+    signal.signal(signal.SIGINT, on_exit)               # Control + C
+    signal.signal(signal.SIGTERM, on_exit)              # kill
     logger.info(f"启动服务：127.0.0.1:8078")
     server.start()
     server.wait_for_termination()
